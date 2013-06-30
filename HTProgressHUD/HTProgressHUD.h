e2bdb41abd0f7b6c981b4bc58335a5063f18a988
hardtack <6566gun@gmail.com>
2013-06-30 21:37:49 +0900

    Initial Commit

---
+++ HTProgressHUD/HTProgressHUD.h
@@ -0,0 +1,13 @@
+//
+//  HTProgressHUD.h
+//  HTProgressHUD
+//
+//  Created by 최건우 on 13. 6. 30..
+//  Copyright (c) 2013년 Hardtack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTProgressHUD : NSObject
+
+@end
---
+++ HTProgressHUD/HTProgressHUD.m
@@ -0,0 +1,13 @@
+//
+//  HTProgressHUD.m
+//  HTProgressHUD
+//
+//  Created by 최건우 on 13. 6. 30..
+//  Copyright (c) 2013년 Hardtack. All rights reserved.
+//
+
+#import "HTProgressHUD.h"
+
+@implementation HTProgressHUD
+
+@end
