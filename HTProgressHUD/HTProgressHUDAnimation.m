620cf370e0dd536b2b295904e1e5b028a3668f1f
GunWoo Choi <6566gun@gmail.com>
2014-07-09 10:02:43 +0900

    Remove copyright comments

--- HTProgressHUD/HTProgressHUDAnimation.m
+++ HTProgressHUD/HTProgressHUDAnimation.m
@@ -3,7 +3,6 @@
 //  HTProgressHUD
 //
 //  Created by 최건우 on 13. 6. 30..
-//  Copyright (c) 2013년 Hardtack. All rights reserved.
 //
 
 #import "HTProgressHUDAnimation.h"
--- HTProgressHUDExample/HTProgressHUDExample/HTAppDelegate.m
+++ HTProgressHUDExample/HTProgressHUDExample/HTAppDelegate.m
@@ -3,7 +3,6 @@
 //  HTProgressHUDExample
 //
 //  Created by 최건우 on 13. 6. 30..
-//  Copyright (c) 2013년 Hardtack. All rights reserved.
 //
 
 #import "HTAppDelegate.h"
